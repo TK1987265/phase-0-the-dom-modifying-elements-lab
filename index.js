b60ef00ddcc3aff43cce95e0dbf92a37c38fc016
TK1987265 <qcc2016@yahoo.com>
2024-04-23 18:34:54 -0400

    javascript -dom-manipulation

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove(); 
+
+// Create an <h1> element
+const newHeader = document.createElement('h1');
+
+// Assign ID 'victory' to the new <h1>
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1>
+newHeader.textContent = 'Mohammad is the champion'; 
+
+// Append the new <h1> to the body
+document.body.appendChild(newHeader);
+
+}
