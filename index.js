163bc3ea4438ab015bd4cb1f72b52c1b9d20cb81
Mohammad Khan <qcc2016@yahoo.com>
2024-05-22 16:51:00 -0400

    added new functions

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const mainElement = document.getElementById('main');
-if (mainElement) {
-    mainElement.remove(); 
+// helpers.js
 
-// Create an <h1> element
-const newHeader = document.createElement('h1');
-
-// Assign ID 'victory' to the new <h1>
-newHeader.id = 'victory';
-
-// Set the text content of the new <h1>
-newHeader.textContent = 'Mohammad is the champion'; 
-
-// Append the new <h1> to the body
-document.body.appendChild(newHeader);
-
-}
+function removeMain() {
+    const mainElement = document.querySelector('main#main');
+    if (mainElement) {
+      mainElement.remove();
+    }
+  }
+  
+  function createHeader() {
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    return newHeader;
+  }
+  
+  function setHeaderText(newHeader, name) {
+    newHeader.innerHTML = `${name} is the champion`;
+  }
+  
+  module.exports = { removeMain, createHeader, setHeaderText };
+  
