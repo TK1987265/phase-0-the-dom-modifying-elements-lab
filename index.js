94447bae2dc055ef65e271eeed6480f79d856669
Mohammad Khan <qcc2016@yahoo.com>
2024-05-25 16:51:24 -0400

    fixed errors

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// helpers.js
 
-function removeMain() {
-    const mainElement = document.querySelector('main#main');
-    if (mainElement) {
-      mainElement.remove();
-    }
-  }
-  
-  function createHeader() {
-    const newHeader = document.createElement('h1');
-    newHeader.id = 'victory';
-    return newHeader;
-  }
-  
-  function setHeaderText(newHeader, name) {
-    newHeader.innerHTML = `${name} is the champion`;
-  }
-  
-  module.exports = { removeMain, createHeader, setHeaderText };
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion";
+
   
